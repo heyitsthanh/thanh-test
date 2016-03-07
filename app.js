8b2e88cd1b8f8b98e0f29a86521c423c1d4df0e0
kahyoung <kahyoung@live.com.au>
2016-03-07 21:26:52 +1100

    Uh oh! Merge Conflict!

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
     res.send('hello again');
 });
 
+app.get('/secret', function(req, res) {
+    res.send('i wanted to show you how web storm handles merging');
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
